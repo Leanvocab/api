432ad8426af75c017711ff80cc1393524d3ae310
Laszlo Heves <heves.laszlo@gmail.com>
2020-09-24 23:43:17 +0100

    feat: use dotenv for PORT env_var

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import cors from 'cors';
 import app from './app';
 
+const DEFAULT_PORT = 80;
+const port = process.env.PORT || DEFAULT_PORT;
+
 app.use(cors());
  
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
 );
